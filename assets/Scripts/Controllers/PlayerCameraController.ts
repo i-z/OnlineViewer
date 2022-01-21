c39eaf02f0ee125ebe4c3aeae51e187ddc6fd01f
Igor Zubkov <igor.zubkov@wn.games>
2022-01-21 19:45:45 +0300

    + label

--- assets/Scripts/Controllers/PlayerCameraController.ts
+++ assets/Scripts/Controllers/PlayerCameraController.ts
@@ -1,5 +1,5 @@
 
-import { _decorator, Component, Node, log, Camera, EventTouch, Vec3, EventMouse, math, MathBase, misc, Vec2, Rect, UITransform, game, director, Canvas, Size, Touch } from 'cc';
+import { _decorator, Component, Node, log, Camera, EventTouch, Vec3, EventMouse, math, MathBase, misc, Vec2, Rect, UITransform, game, director, Canvas, Size, Touch, Label } from 'cc';
 import { Bounds } from '../Components/Bounds';
 import { ScrollInput, ScrollInputEventType } from '../Components/ScrollInput';
 const { ccclass, property } = _decorator;
@@ -36,6 +36,8 @@ export class PlayerCameraController extends Component {
     scrollInput: ScrollInput = null;
     @property
     limitCamera: boolean = true;
+    @property(Label)
+    currentScale: Label = null;
 
     private _zoomMax: number = 800;
     private _startPosition: Vec2;
@@ -53,6 +55,7 @@ export class PlayerCameraController extends Component {
     private _previousDistance: number = 0;
     private _touches: Touch[] = [];
     private _fistTouchId: number = -1;
+    private _canvasTransform: UITransform = null;
 
     public get worldMousePosition(): Vec2 {
         const p = this.camera.screenToWorld(new Vec3(this._mousePosition.x, this._mousePosition.y, 0));
@@ -67,10 +70,10 @@ export class PlayerCameraController extends Component {
             this.touchInputPanel.on(Node.EventType.MOUSE_WHEEL, this.onMouseWheel, this, true);
             this.touchInputPanel.on(Node.EventType.MOUSE_MOVE, this.onMouseMove, this, true);
         }
-        this._scrollOrtographicSize = this.camera.orthoHeight;
+        this._scrollOrtographicSize = this.orthoHeight;
 
-        const ctr = this.canvas.getComponent(UITransform);
-        this._aspectRatio = (ctr.width / 2) / this.camera.orthoHeight;
+        this._canvasTransform = this.canvas.getComponent(UITransform);
+        this._aspectRatio = (this._canvasTransform.width / 2) / this.orthoHeight;
 
         const tr = this.limits.getComponent(UITransform);
         this._cameraLimits = new Bounds(this.limits.position.x - tr.width * tr.anchorX, this.limits.position.y - tr.height * tr.anchorY, tr.width, tr.height);
@@ -80,9 +83,9 @@ export class PlayerCameraController extends Component {
         this.scrollInput.max = this._zoomMax;
         this.scrollInput.updateSize();
         this.scrollInput.node.on(ScrollInputEventType.UPDATE_Y, (val: number) => {
-            this.camera.orthoHeight = val;
+            this.orthoHeight = val;
         });
-        this.scrollInput.valueY = this.camera.orthoHeight;
+        this.scrollInput.valueY = this.orthoHeight;
     }
 
     scrollBy(d: number, zoomToMouse: boolean) {
@@ -187,17 +190,17 @@ export class PlayerCameraController extends Component {
     }
 
     zoom(deltaTime: number) {
-        let delta: number = this._scrollOrtographicSize - this.camera.orthoHeight;
+        let delta: number = this._scrollOrtographicSize - this.orthoHeight;
         let dir: number = Math.sign(delta);
         let inc: number = deltaTime * 1 / this.zoomDuration * this._scrollOrtographicSize / (dir / 2 + 1.5);
-        this.camera.orthoHeight += dir * inc;
-        delta = this._scrollOrtographicSize - this.camera.orthoHeight;
+        this.orthoHeight += dir * inc;
+        delta = this._scrollOrtographicSize - this.orthoHeight;
         if (this._zoomPanDirection.x != 0 || this._zoomPanDirection.y != 0) {
             const v = Vec3.multiplyScalar(new Vec3(), this._zoomPanDirection, inc);
             this.camera.node.position = this.camera.node.position.add(Vec3.multiplyScalar(new Vec3(), this._zoomPanDirection, inc));
         }
         if (delta > - this._epsilon && delta < this._epsilon || dir != Math.sign(delta)) {
-            this.camera.orthoHeight = this._scrollOrtographicSize;
+            this.orthoHeight = this._scrollOrtographicSize;
             this._zoomPanDirection = new Vec3();
             this._zooming = false;
         }
@@ -221,10 +224,10 @@ export class PlayerCameraController extends Component {
         }
 
         if (this.limitCamera) {
-            if (2 * this.camera.orthoHeight * this._aspectRatio >= this._cameraLimits.width) {
+            if (2 * this.orthoHeight * this._aspectRatio >= this._cameraLimits.width) {
                 this._currentPanSpeed.x = 0 - this.camera.node.position.x;
             }
-            if (2 * this.camera.orthoHeight >= this._cameraLimits.height) {
+            if (2 * this.orthoHeight >= this._cameraLimits.height) {
                 this._currentPanSpeed.y = 0 - this.camera.node.position.y;
             }
         }
@@ -233,7 +236,7 @@ export class PlayerCameraController extends Component {
         this.camera.node.position.add(new Vec3(this._currentPanSpeed.x, this._currentPanSpeed.y, 0).multiplyScalar(positionMultiplier));
 
         const lim = new Bounds(this._cameraLimits);
-        lim.contentSize = lim.contentSize.subtract(new Vec2(2 * this.camera.orthoHeight * this._aspectRatio, 2 * this.camera.orthoHeight));
+        lim.contentSize = lim.contentSize.subtract(new Vec2(2 * this.orthoHeight * this._aspectRatio, 2 * this.orthoHeight));
         if (lim.size.x < 0) {
             const sz = lim.size;
             sz.x = 0;
@@ -263,8 +266,8 @@ export class PlayerCameraController extends Component {
             const distance = Vec2.distance(this._touches[0].getLocation(), this._touches[1].getLocation());
             const delta = distance - this._previousDistance;
             if ((delta > this._epsilon || delta < - this._epsilon) && this._previousDistance != 0) {
-                this.camera.orthoHeight *= this._previousDistance / distance;
-                this.camera.orthoHeight = misc.clampf(this.camera.orthoHeight, this.zoomMin, this._zoomMax);
+                this.orthoHeight *= this._previousDistance / distance;
+                this.orthoHeight = misc.clampf(this.orthoHeight, this.zoomMin, this._zoomMax);
             }
             this._previousDistance = distance;
         }
@@ -296,5 +299,14 @@ export class PlayerCameraController extends Component {
         this._mousePosition.x = event.getLocationX();
         this._mousePosition.y = event.getLocationY();
     }
+
+    public get orthoHeight() : number {
+        return this.camera.orthoHeight;
+    }
+    public set orthoHeight(v : number) {
+        this.camera.orthoHeight = v;
+        this.currentScale.string = `${ Math.round(this._canvasTransform.height * 100 / this.camera.orthoHeight) }%`
+    }
+    
 }
 
